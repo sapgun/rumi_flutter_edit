ab4f1003c18a61b7418a46fad3bcb594afc0b36a
BangHyeok <qkdgur0323@gmail.com>
2023-11-27 15:27:33 +0900

    날씨추가

---
+++ lib/loading.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:senior_fitness_app/dashboard.dart';
+import 'package:senior_fitness_app/data/my_location.dart';
+import 'package:senior_fitness_app/data/network.dart';
+
+const apiKey = '0d0cc1131b44cd6ea0027e60e69dc007';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  double? latitude3;
+  double? longitude3;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    MyLocation myLocation = MyLocation();
+    await myLocation.getMyCurrentLocation();
+    latitude3 = myLocation.latitude2;
+    longitude3 = myLocation.longitude2;
+
+    Network network = Network(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric',
+        'https://api.openweathermap.org/data/2.5/air_pollution?lat=$latitude3&lon=$longitude3&appid=$apiKey');
+    var weatherData = await network.getJsonData();
+    print(weatherData);
+
+    var airData = await network.getAirData();
+    print(airData);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return Dashboard(
+        parseWeatherData: weatherData,
+        parseAirPollution: airData,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: null,
+          child: Text(
+            'Get my location',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+// void fetchData() async{
+//
+//     var myJson = parsingData['weather'][0]['description'];
+//     print(myJson);
+//
+//     var wind = parsingData['wind']['speed'];
+//     print(wind);
+//
+//     var id =parsingData['id'];
+//     print(id);
+//   }else{
+//     print(response.statusCode);
+//   }
+// }
