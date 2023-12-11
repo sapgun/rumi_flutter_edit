e7bddef291c0db1e7495a052f534395dc2b9212e
jhpark0323 <brianwogus@naver.com>
2023-12-11 11:34:05 +0900

    error

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import flutter_tts
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
@@ -16,7 +15,6 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,14 +3,13 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  file_selector_windows
-  flutter_tts
-  geolocator_windows
-  url_launcher_windows
-)
+        file_selector_windows
+        geolocator_windows
+        url_launcher_windows
+        )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-)
+        )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
